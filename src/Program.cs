c00ed84c6951d66d1fff5bad21dbb9c5e063b4f6
GooRiOn <foreverframenet@gmail.com>
2020-06-12 09:09:30 +0200

    Added source code

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)  
+        {
+            
+        }
+    }
+}
