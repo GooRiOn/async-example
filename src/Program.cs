5e5928dd6efa05fb9aba98c2c45e3a012d64a9ad
GooRiOn <foreverframenet@gmail.com>
2020-06-12 09:10:22 +0200

    Added Task.WhenAll

--- src/Program.cs
+++ src/Program.cs
@@ -10,8 +10,27 @@ namespace AsyncExample
     class Program
     {
         static async Task Main(string[] args)  
-        {
-            
+        {  
+        var order1Ticket = MakeOrderAsync(1, "Cheeseburger", "Hamburger"); 
+        
+        Console.WriteLine("Let's do some other order in a meantime....");
+        var order2Ticket = MakeOrderAsync(2, "Cheeseburger", "FishMac", "BigMac", "McRoyal");
+        
+        Console.WriteLine("Again...let's do some other order in a meantime...."); 
+        var order3Ticket = MakeOrderAsync(3, "Hamburger"); 
+
+        await Task.WhenAll(order1Ticket, order2Ticket, order3Ticket);
+        Console.WriteLine("All orders completed!");
+        }  
+        
+        static async Task<IEnumerable<string>> MakeOrderAsync(int orderNumber, params string[] burgers)  
+        {  
+        var orderProcessingTime = burgers.Length * 1000;  
+        Console.WriteLine($"Preparing order #{orderNumber}... ETA: {orderProcessingTime} ms");  
+        await Task.Delay(orderProcessingTime);  
+        Console.WriteLine($"Order #{orderNumber} ready!");  
+
+        return burgers;
         }
     }
 }
